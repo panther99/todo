6c7bc24287e09f25c7b7d0b348491593ea93ea07
macak99 <nikola.stojakovic@hotmail.com>
2015-08-10 20:18:24 +0200

    Added function escape() in init.php to prevent XSS

--- app/init.php
+++ app/init.php
@@ -4,10 +4,14 @@ session_start();
 
 $_SESSION['user_id'] = 1;
 
-$db = new PDO('mysql:dbname=database_name;host=localhost', 'user', 'password');
+$db = new PDO('mysql:dbname=todo;host=localhost', 'user', 'password');
 
 if(!isset($_SESSION['user_id'])) {
 	die('You\'re not signed in!');
+}
+
+function escape($string) {
+	return htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
 }
 
 ?>
